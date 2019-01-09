c8d030e4f7acf82d904352f91be035ed64efb4c4
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2019-01-09 17:19:59 +0300

    убрал мусор

--- js/backend.js
+++ js/backend.js
@@ -44,60 +44,6 @@
     xhr.open('GET', loadURL);
     xhr.send();
   };
-  /*
-  var upload = function (data, onLoad, onError) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-
-    xhr.open('POST', uploadURL);
-    xhr.send(data);
-  };
-
-  var load = function (onLoad, onError) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-
-    xhr.open('GET', loadURL);
-    xhr.send();
-  };*/
 
   window.backend = {
     upload: upload,
